426427c59176f3e801e9b4eed9540bdabbce0ea1
wowissu <bbgbbg0123@gmail.com>
2021-07-06 16:31:45 +0800

    feat(eslint): flat babel rules

--- eslint/babel/index.js
+++ eslint/babel/index.js
@@ -1,13 +1,4 @@
 /** @type { import('eslint').Linter.Config } */
 module.exports = {
-  overrides: [
-    {
-      files: ['*.js'],
-      env: {
-        node: true,
-        es6: true
-      },
-      parser: 'babel-eslint'
-    }
-  ]
+  parser: 'babel-eslint',
 };
