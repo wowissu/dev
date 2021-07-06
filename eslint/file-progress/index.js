34ceb69786e4f6117d1d40b00ba80efb9918d6f0
wowissu <bbgbbg0123@gmail.com>
2021-07-06 13:15:19 +0800

    lint: use eslint as root dir.

---
+++ eslint/file-progress/index.js
@@ -0,0 +1,15 @@
+/**
+ * show progress in process
+ */
+
+/** @type { import('eslint').Linter.Config } */
+module.exports = {
+  plugins: [
+    'file-progress'
+  ],
+
+  // add your custom rules here
+  rules: {
+    'file-progress/activate': 1
+  }
+};
---
+++ eslint/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  babel: require('./babel'),
+  common: require('./common'),
+  'file-progress': require('./file-progress'),
+  ts: require('./ts'),
+  vue: require('./vue'),
+}
