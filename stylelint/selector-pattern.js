421df1cc53d44a2817485d9483c65337ad7bd012
wowissu <bbgbbg0123@gmail.com>
2021-07-15 19:48:43 +0800

    fix(stylelint): ex to eg, lol

--- stylelint/selector-pattern.js
+++ stylelint/selector-pattern.js
@@ -3,14 +3,14 @@
 module.exports = {
   rules: {
     "selector-class-pattern": ["^([a-z0-9]{1}[a-zA-Z0-9]{0,}(\\_{2})?)+([a-zA-Z]{0,}\\-{2}[a-z0-9]{1}[a-zA-Z0-9]{0,}){0,}$", {
-      message: "please follow as ex: .foo, .foo__bar, .foo--bar",
+      message: "please follow as eg: .foo, .foo__bar, .foo--bar",
       resolveNestedSelectors: true
     }],
     "selector-id-pattern": ["([a-z]+[a-zA-Z0-9]{0,}(\\-[a-z0-9]+)+)", {
-      message: "please follow as ex: #foo, #foo-bar",
+      message: "please follow as eg: #foo, #foo-bar",
     }],
     "keyframes-name-pattern": ["([a-z]+[a-zA-Z0-9]{0,}(\\-[a-z0-9]+)+)", {
-      message: "please follow as ex: @keyframes foo {}, @keyframes foo-bar {}",
+      message: "please follow as eg: @keyframes foo {}, @keyframes foo-bar {}",
     }]
   }
 }
