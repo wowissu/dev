094508af0e6d116e0b71a756972d2b51d47a7333
wowissu <bbgbbg0123@gmail.com>
2021-07-15 17:55:01 +0800

    feat(stylelint): add rule-empty-line-before rules

--- stylelint/recommended.js
+++ stylelint/recommended.js
@@ -3,5 +3,10 @@
 module.exports = {
   extends: [
     "stylelint-config-recommended",
-  ]
+  ],
+  rules: {
+    "rule-empty-line-before": ["always-multi-line", {
+      "except": ["after-single-line-comment", "first-nested"]
+    }]
+  }
 }
