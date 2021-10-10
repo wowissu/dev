06ac4e3694e64ec7c8695a540bdfadb17b166ab9
wowissu <bbgbbg0123@gmail.com>
2021-10-10 21:09:11 +0800

    feat(eslint): vue3 add globals variable

--- eslint/vue/vue3.js
+++ eslint/vue/vue3.js
@@ -3,5 +3,11 @@ module.exports = {
   extends: [
     'plugin:vue/vue3-recommended',
     './rules.js'
-  ]
+  ],
+  globals: {
+    defineProps: "readonly",
+    defineEmits: "readonly",
+    defineExpose: "readonly",
+    withDefaults: "readonly",
+  }
 };
