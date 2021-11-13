e293e43a7f80f76199c8f66b0c74017d18d7329f
wowissu <bbgbbg0123@gmail.com>
2021-11-13 09:43:58 +0800

    test(eslint): update test content

--- eslint/test.js
+++ eslint/test.js
@@ -1,11 +1,13 @@
-const fs = require('fs');
 const eslint = require('eslint');
 const glob = require('glob');
 const path = require('path');
+const expect = require('chai').expect;
 
 describe ('Validate ESLint configs', () => {
 
   async function getErrors(configFile, content) {
+    console.log(configFile);
+
     const cli = new eslint.ESLint({
       overrideConfigFile: configFile,
       useEslintrc: false,
@@ -16,15 +18,28 @@ describe ('Validate ESLint configs', () => {
     return eslint.ESLint.getErrorResults(results);
   }
 
-  const attrs = glob.sync(path.join(__dirname, '/!(node_modules)/*.js')).map(f => [f, ''])
+  const files = glob.sync(path.join(__dirname, '/!(node_modules)/**/*.js'));
 
-  test.each(attrs)('load config %s in ESLint to validate all rules are correct', async (fname, textContent)=>{
-    try {
-      const errs = await getErrors(fname, textContent);
+  const textContent = '';
 
-      expect(errs.length).toEqual(0);
-    } catch (err) {
-      throw err;
-    }
-  });
+  files.forEach((fname) => {
+    it(`load config ${fname} in ESLint to validate all rules are correct`, (done)=>{
+      getErrors(fname, textContent)
+        .then((errs) => {
+
+          if (errs.length) {
+            return Promise.reject(errs[0])
+          } else {
+            expect(errs.length).to.eql(0);
+          }
+
+          done();
+        })
+        .catch((err) => {
+          console.log(err.messages.map((m)=>`[${m.ruleId}]: ${m.message}`));
+
+          done(new Error(`Got ${err.messages.length} error message.`));
+        })
+    })
+  })
 });
