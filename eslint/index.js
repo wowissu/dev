439c506f694c6c41e379471a14f6e38e7f257825
wowissu <bbgbbg0123@gmail.com>
2022-05-27 12:02:21 +0800

    fix(eslint): @babel should install @babel/core in deps

--- eslint/index.js
+++ eslint/index.js
@@ -1,8 +1,9 @@
 module.exports = {
+  "@babel": require('./@babel'),
   babel: require('./babel'),
   common: require('./common'),
   fileProgress: require('./file-progress'),
   ts: require('./ts'),
   vue: require('./vue'),
-  vue3: require('./vue3'),
+  vue3: require('./vue/vue3'),
 }
--- eslint/ts/base.js
+++ eslint/ts/base.js
@@ -1,13 +1,6 @@
 /** @return { import('eslint').Linter.Config } */
 module.exports = {
   extends: [
-    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended'
-  ],
-  plugins: [
-    '@typescript-eslint'
-  ],
-  parserOptions: {
-    parser: '@typescript-eslint/parser'
-  }
+  ]
 };
