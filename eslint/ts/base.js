620493f3f04a507f4d946b8208eab88a75fe5b27
wowissu <bbgbbg0123@gmail.com>
2022-08-11 15:36:22 +0800

    feat(eslint/ts): add all formatting rules

--- eslint/ts/base.js
+++ eslint/ts/base.js
@@ -25,6 +25,9 @@ module.exports = {
       }
     ],
 
+    "comma-spacing": "off",
+    "@typescript-eslint/comma-spacing": ["error"],
+
     "indent": "off",
     "@typescript-eslint/indent": ["error", 2],
 
@@ -32,8 +35,29 @@ module.exports = {
     "@typescript-eslint/object-curly-spacing": ["error", "always", { arraysInObjects: false }],
 
     "space-before-blocks": "off",
-    "@typescript-eslint/space-before-blocks": "error",
+    "@typescript-eslint/space-before-blocks": ["error", { "before": false, "after": true }],
 
-    "@typescript-eslint/type-annotation-spacing": ["error", { "before": false, "after": true }]
+    "space-before-function-paren": "off",
+    "@typescript-eslint/space-before-function-paren": ["error", { "anonymous": "always", "named": "never", "asyncArrow": "always" }],
+
+    "func-call-spacing": "off",
+    "@typescript-eslint/func-call-spacing": ["error"],
+
+    "keyword-spacing": "off",
+    "@typescript-eslint/keyword-spacing": ["error", { "after": true, "before": true }],
+
+    "padding-line-between-statements": "off",
+    "@typescript-eslint/padding-line-between-statements": [
+      "error",
+      { "blankLine": "always", "prev": "*", "next": ["if", "switch", "do", "while", "for", "debugger", "function"]},
+      { "blankLine": "always", "prev": ["if", "switch", "do", "while", "for", "debugger", "function"], "next": "*" },
+      { "blankLine": "always", "prev": ["import"], "next": "*" },
+      { "blankLine": "any", "prev": ["import"], "next": ["import"]}
+    ],
+
+    "space-infix-ops": "off",
+    "@typescript-eslint/space-infix-ops": ["error", { "int32Hint": false }],
+
+    "@typescript-eslint/type-annotation-spacing": ["error", { "before": false, "after": true, "overrides": { "arrow": { "before": true, "after": true } } }]
   }
 };
